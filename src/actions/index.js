ef8ccffc96da919622dc18b27e8d71efe147fb5f
nasnick <nas.nick@gmail.com>
2016-12-05 22:46:27 +1300

    Build out containers/weather_list

--- src/actions/index.js
+++ src/actions/index.js
@@ -9,6 +9,9 @@ export const FETCH_WEATHER = 'FETCH_WEATHER';
 export function fetchWeather(city) {
 	const url = `${ROOT_URL}&q=${city},nz`;
 	const request = axios.get(url);
+
+    // console.log('Request: ' request);
+
 	return{
       type: FETCH_WEATHER,
       payload: request
